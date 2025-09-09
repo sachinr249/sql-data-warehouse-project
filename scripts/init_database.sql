b0ec47e92271bfd30b829d58709319db14ab73c3
sachin mewara <sachin421pwian@gmail.com>
2025-09-09 21:31:03 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,25 @@
+/* 
+==========================================
+create database and schemas
+==========================================
+script purpose:
+      This scrit create a new database "DataWarehouse" with schemas 'bronze','silver' and 'gold'
+*/
 
+use master;
+go
+
+--create the 'DataWarehouse' database
+create database DataWarehouse ;
+go
+
+use DataWarehouse ;
+go
+
+-- create schemas
+create schema bronze ;
+go
+create schema silver ;
+go
+create schema gold ;
+go
